3f9f02f862fc69d72edb00164b3f13da57a9a111
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-06-01 20:28:14 +0900

    wip

--- src/entrypoints/sidepanel/root.tsx
+++ src/entrypoints/sidepanel/root.tsx
@@ -1,5 +1,17 @@
 import React from "react";
 
+// TODO:
+// - settings
+//   - default language pair
+// - storage
+//   - cache last loaded subtitles
+// - monitor active tab video
+//   - refresh
+//   - disable when not
+// - fetch metadata
+// - subtitles UI
+// - abstract service interface (context/provider?)
+
 export function Root() {
 	return (
 		<div>
