0eadf36ff169d07a0b4b6121a56720ada4458b69
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-06-07 18:51:31 +0900

    chore: cleanup

--- src/entrypoints/background/index.ts
+++
@@ -1,7 +0,0 @@
-import { defineBackground } from "wxt/utils/define-background";
-
-export default defineBackground({
-	main() {
-		import("./main");
-	},
-});
--- src/entrypoints/background/main.ts
+++
@@ -1,9 +0,0 @@
-import { browser } from "wxt/browser";
-
-async function main() {
-	console.log("Hello background!", { id: browser.runtime.id });
-}
-
-main();
-
-export {};
