771ece14b8a2c50ed0816996483a467003af2575
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-06-08 14:35:09 +0900

    refactor: control UI in content

--- src/entrypoints/content-iframe/main.tsx
+++ src/entrypoints/content-iframe/main.tsx
@@ -1,10 +1,14 @@
 import "../../styles.css";
 import ReactDomClient from "react-dom/client";
 import { Root } from "./root";
+import { RootControl } from "./root-control";
+
+const uiParams = new URL(window.location.href).searchParams;
+const isControl = uiParams.has("control");
 
 async function main() {
 	const dom = document.querySelector("#root")!;
-	const vdom = <Root />;
+	const vdom = isControl ? <RootControl /> : <Root />;
 	ReactDomClient.createRoot(dom).render(vdom);
 }
 
